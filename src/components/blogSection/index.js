001902dadf601a919777dfa3b688b15cce0af273
Johan Guse <johanguse@gmail.com>
2020-02-10 14:09:18 -0300

    Custom arrow slick slider svg

--- src/components/blogSection/index.js
+++ src/components/blogSection/index.js
@@ -2,6 +2,29 @@ import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import Slider from "react-slick"
 import Img from "gatsby-image"
+import './_blogSection.scss';
+
+function SlickNextArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <button type='button' className='slick-next pull-right'
+    onClick={onClick}
+    >
+      <svg xmlns='http://www.w3.org/2000/svg' width='60' height='60' viewBox='0 0 45 45'><path fill='#4a90e2' d='M14.063 10.145l1.82-1.707L30.938 22.5 15.883 36.563l-1.82-1.696L27.288 22.5zm0 0'/></svg>
+    </button>
+  );
+}
+
+function SlickPrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <button type='button' className='slick-prev pull-left'
+    onClick={onClick}
+    >
+      <svg xmlns='http://www.w3.org/2000/svg' width='60' height='60' viewBox='0 0 45 45'><path fill='#4a90e2' d='M30.938 10.145l-1.82-1.707L14.062 22.5l15.054 14.063 1.82-1.696L17.712 22.5zm0 0'/></svg>
+    </button>
+  );
+}
 
 
 function BlogSection() {
@@ -10,7 +33,20 @@ function BlogSection() {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: <SlickNextArrow />,
+    prevArrow: <SlickPrevArrow />,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
   };
   return (
     <StaticQuery
